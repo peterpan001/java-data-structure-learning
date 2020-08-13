d5f9a2fea3480025f6b8e5538a457fb5d2a466e5
panli004 <panli004@ke.com>
2020-08-13 10:27:15 +0800

    冒泡排序与插入排序算法

---
+++ data-structure-learning-sort/src/main/java/com/panli/sort/bubble/Main.java
@@ -0,0 +1,60 @@
+package com.panli.sort.bubble;
+
+/**
+ * @author lipan
+ * @date 2020-08-12
+ * @desc 冒泡排序
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 5, 2, 6, 3, 9, 8, 7};
+
+        new Main().bubbleSort(nums);
+
+        for (int tmp : nums) {
+            System.out.println(tmp);
+        }
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param nums
+     */
+    public void bubbleSort(int[] nums) {
+
+        if (nums.length <= 1) {
+            return;
+        }
+
+        // 定义boolean变量，标记每轮遍历中是否发生了数组元素交换
+        boolean hasChange = true;
+
+        // 每轮遍历开始都将hasChange置为false
+        for (int i = 0; i < nums.length - 1 && hasChange; i++) {
+            hasChange = false;
+            for (int j = 0; j < nums.length - i - 1; j++) {
+
+                // 进行两两比较，如果发现当前数比下一个数大时，进行交换，并将hasChange置为true
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                    hasChange = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * 交换数组元素位置
+     *
+     * @param nums
+     * @param i
+     * @param j
+     */
+    public void swap(int nums[], int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
---
+++ data-structure-learning-sort/src/main/java/com/panli/sort/insertion/Main.java
@@ -0,0 +1,34 @@
+package com.panli.sort.insertion;
+
+/**
+ * @author lipan
+ * @date 2020-08-12
+ * @desc 插入排序
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 3, 6, 2, 8, 7};
+        new Main().insertionSort(nums);
+        for (int tmp : nums) {
+            System.out.println(tmp);
+        }
+    }
+
+    public void insertionSort(int[] nums) {
+        if (nums.length <= 1) {
+            return;
+        }
+        // 假设第一个数是已经拍好序的，故从第二个数开始遍历数组
+        for (int i = 1, j; i < nums.length; i++) {
+            // 外围开始循环，把当前i指向的值用current保存
+            int current = nums[i];
+            // 指针 j 内循环，和 current 值比较，若 j 所指向的值比 current 值大，则该数右移一位
+            for (j = i - 1; j >= 0 && nums[j] > current; j--) {
+                nums[j + 1] = nums[j];
+            }
+            // 内循环结束，j+1 所指向的位置就是 current 值插入的位置
+            nums[j + 1] = current;
+        }
+    }
+}
