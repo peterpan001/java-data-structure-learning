4b593e4842bbdb92ec0bddc08e6119d58d236937
panli004 <panli004@ke.com>
2020-08-17 21:07:19 +0800

    队列模拟栈&栈模拟队列添加注释

--- data-structure-learning-queue/src/main/java/com/panli/queue/stack/QueueToStack.java
+++ data-structure-learning-queue/src/main/java/com/panli/queue/stack/QueueToStack.java
@@ -7,6 +7,7 @@ import java.util.Queue;
  * @author lipan
  * @date 2020-08-17
  * @desc 两个队列模拟栈
+ * 队列：先进先出；栈：先进后出
  */
 public class QueueToStack {
 
@@ -15,10 +16,12 @@ public class QueueToStack {
 
     // 队列模拟进栈
     public void push(int item) {
-        if (queue1.isEmpty() && queue2.isEmpty()) {
+        if (queue1.isEmpty() && queue2.isEmpty()) { //如果两个队列都为空，则选择queue1压入元素
             queue1.add(item);
             return;
         }
+
+        // 判断哪个队列不为空，就一直向其压入元素
         if (queue1.isEmpty()) {
             queue2.add(item);
             return;
@@ -31,9 +34,11 @@ public class QueueToStack {
 
     // 队列模拟出栈
     public int pop() {
-        if (queue1.isEmpty() && queue2.isEmpty()) {
+        if (queue1.isEmpty() && queue2.isEmpty()) { //如果两个队列都为空，则返回-1
             return -1;
         }
+
+        // 判断哪个队列不为空，就将其n-1个元素压入另一个队列，最后一个元素直接出队列即可
         if (queue1.isEmpty()) {
             while (queue2.size() > 1) {
                 queue1.add(queue2.poll());
--- data-structure-learning-stack/src/main/java/com/panli/stack/queue/StackToQueue.java
+++ data-structure-learning-stack/src/main/java/com/panli/stack/queue/StackToQueue.java
@@ -6,6 +6,7 @@ import java.util.Stack;
  * @author lipan
  * @date 2020-08-17
  * @desc 借助于两个栈模拟对列
+ * 栈：先进后出；队列：先进先出
  */
 public class StackToQueue {
 
@@ -14,10 +15,12 @@ public class StackToQueue {
 
     // 栈实现元素进队列
     public void add(int item) {
-        if (stack1.isEmpty() && stack2.isEmpty()) {
+        if (stack1.isEmpty() && stack2.isEmpty()) { // 如果栈都为空，选择stack1入栈
             stack1.push(item);
             return;
         }
+
+        //判断哪个栈不为空，就一直向其压入元素
         if (stack1.isEmpty()) {
             stack2.push(item);
         }
@@ -28,9 +31,11 @@ public class StackToQueue {
 
     // 栈实现元素出队列
     public int poll() {
-        if (stack1.isEmpty() && stack2.isEmpty()) {
+        if (stack1.isEmpty() && stack2.isEmpty()) { //如果两个栈都为空，则返回-1
             return -1;
         }
+
+        // 判断哪个栈不为空，就将其n-1个元素压入另一个栈，最后一个元素直接出栈即可
         if (stack1.isEmpty()) {
             while (stack2.size() > 1) {
                 stack1.push(stack2.pop());
