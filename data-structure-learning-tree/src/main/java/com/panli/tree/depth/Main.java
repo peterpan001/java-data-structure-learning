728ff40db46d8242bf47883e87788a2dc3fc2bc9
panli004 <panli004@ke.com>
2020-08-17 20:39:24 +0800

    二叉树深度求解

--- data-structure-learning-tree/src/main/java/com/panli/tree/depth/Main.java
+++ data-structure-learning-tree/src/main/java/com/panli/tree/depth/Main.java
@@ -26,6 +26,7 @@ public class Main {
         return (nLeft > nRight) ? nLeft + 1 : nRight + 1;
     }
 
+    // 二叉树非递归求解树的深度
     public static int treeDepthNoRecursion(BinaryTree pRoot) {
         if (pRoot == null) {
             return 0;
