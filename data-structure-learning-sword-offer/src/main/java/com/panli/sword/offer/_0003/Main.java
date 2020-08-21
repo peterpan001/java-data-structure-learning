7bd60570a1125027365aed25aca9410da00f30c4
panli004 <panli004@ke.com>
2020-08-21 09:03:28 +0800

    sword-offer 数组中重复的数字

---
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0003/Main.java
@@ -0,0 +1,24 @@
+package com.panli.sword.offer._0003;
+
+import java.util.HashSet;
+
+/**
+ * @author lipan
+ * @date 2020-08-21
+ * @desc 数组中重复的数字
+ */
+public class Main {
+
+
+    // 借助 HashSet，每个元素只能放一个，如果set集合中包含此元素则直接返回即可
+    public int findRepeatNumber(int[] nums) {
+        HashSet<Integer> sets = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (sets.contains(nums[i])) {
+                return nums[i];
+            }
+            sets.add(nums[i]);
+        }
+        return -1;
+    }
+}
