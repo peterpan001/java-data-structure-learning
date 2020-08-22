7d728c256b012f047a68188384634e0af4de6fc7
panli004 <panli004@ke.com>
2020-08-22 10:41:25 +0800

    二分查找save

--- data-structure-learning-search/src/main/java/com/panli/search/binary/Main.java
+++ data-structure-learning-search/src/main/java/com/panli/search/binary/Main.java
@@ -9,33 +9,22 @@ public class Main {
 
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5, 6, 7};
-        int no = new Main().binarySearch1(nums, 1);
-        int no1 = new Main().binarySearch1(nums, 5);
-        int no2 = new Main().binarySearch1(nums, 7);
+        int no = new Main().binarySearch(nums, 1);
+        int no1 = new Main().binarySearch(nums, 5);
+        int no2 = new Main().binarySearch(nums, 7);
         System.out.println(no);
         System.out.println(no1);
         System.out.println(no2);
     }
 
+    /**
+     * 二分查找算法
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int binarySearch(int nums[], int target) {
-        int ins, low = 0, high = nums.length - 1;
-        while (true) {
-            ins = (low + high) / 2; //取数组中间元素下标
-            if (nums[ins] == target) { // 与目标元素进行比较
-                return ins;
-            } else if (low > high) {
-                return -1;
-            } else {
-                if (nums[ins] > target) { //如果大于目标元素，则在左半部分查找
-                    high = ins - 1;
-                } else { // 在右半部分查找
-                    low = ins + 1;
-                }
-            }
-        }
-    }
-
-    public int binarySearch1(int nums[], int target) {
         int mid, low = 0, high = nums.length - 1;
         while (low <= high) {
             mid = (low + high) / 2;
