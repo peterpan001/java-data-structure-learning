a27ac94bddd962d0e9a220af71c455752415fed5
panli004 <panli004@ke.com>
2020-08-24 10:04:33 +0800

    sword-offer:重建二叉树

---
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/domain/TreeNode.java
@@ -0,0 +1,15 @@
+package com.panli.sword.offer.domain;
+
+/**
+ * @author lipan
+ * @date 2020-08-23
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
