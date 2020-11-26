07831b731865f2ac11fce598038cb174f42c68f3
panli004 <panli004@ke.com>
2020-11-26 08:25:54 +0800

    sword offer 19

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0019/Main.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0019/Main.java
@@ -7,17 +7,17 @@ package com.panli.sword.offer._0019;
  */
 public class Main {
 
-    boolean isMatch(String s, String p) {
+    public boolean isMatch(String s, String p) {
         if (p.isEmpty()) { // 在p为空后，只需要看s是否为空即可；是：匹配，否则不匹配
             return s.isEmpty();
         }
         // 查看首元素是否一致
         boolean first_match = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
 
-        //如果下一个元素是*
+        //如果下一个元素是*，两种情况，1：从s串开头重新匹配；2：从s串第二位开始匹配，相当于p的第一个字符因为有*可以无限多个
         if (p.length() >= 2 && p.charAt(1) == '*') {
             return isMatch(s, p.substring(2)) || (first_match && isMatch(s.substring(1), p));
-        } else {
+        } else { // 比较s的第二个字符与p的第二个字符
             return first_match && isMatch(s.substring(1), p.substring(1));
         }
     }
