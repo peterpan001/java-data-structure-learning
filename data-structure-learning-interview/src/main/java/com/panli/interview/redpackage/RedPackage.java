5087e47fe421bf5e189d1a1450faf8e190510108
panli004 <panli004@ke.com>
2020-12-07 08:56:08 +0800

    sword-offer: redpackage

---
+++ data-structure-learning-interview/src/main/java/com/panli/interview/redpackage/RedPackage.java
@@ -0,0 +1,53 @@
+package com.panli.interview.redpackage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author lipan
+ * @date 2020-12-07
+ */
+public class RedPackage {
+
+    public static void main(String[] args) {
+        RedPackage redPackage = new RedPackage();
+        List<Integer> list = redPackage.getRandomRedPackageList(100, 10);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public List<Integer> getRandomRedPackageList(int totalMoney, int totalPerson) {
+        // 存储随机红包列表
+        List<Integer> redList = new ArrayList<Integer>();
+
+        // 参数校验
+        if (totalMoney <= 0 || totalPerson <= 0) {
+            return redList;
+        }
+
+        if (totalPerson == 1) {
+            redList.add(totalMoney);
+            return redList;
+        }
+
+        // 随机种子
+        Random random = new Random();
+        // 随机分配给前n-1个人
+        for (int i = 0; i < totalPerson - 1; i++) {
+            int randomMoney = 0;
+            while (true) {
+                randomMoney = random.nextInt(totalMoney / (totalPerson - i) * 2 - 1) + 1;
+                if (randomMoney <= totalMoney * 0.9) {
+                    break;
+                }
+            }
+            totalMoney -= randomMoney;
+            redList.add(randomMoney);
+        }
+        // 剩余的给最后一个人
+        redList.add(totalMoney);
+        return redList;
+    }
+}
