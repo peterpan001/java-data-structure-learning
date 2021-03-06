27d29766f9446f334930874d5008646fdf9a9529
panli004 <panli004@ke.com>
2021-03-06 18:36:25 +0800

    sword-offer: 和为s的两个数字

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0057_I/Main.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0057_I/Main.java
@@ -7,4 +7,30 @@ package com.panli.sword.offer._0057_I;
  * @date 2021-02-23
  */
 public class Main {
+
+    /**
+     * 双指针解法
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || target <= 0) {
+            return new int[0];
+        }
+        int lowIdx = 0;
+        int highIdx = nums.length - 1;
+        while (lowIdx < highIdx) {
+            int res = nums[lowIdx] + nums[highIdx];
+            if (res > target) { //两数之和 > 目标值，大数指针前移
+                highIdx--;
+            } else if (res < target) { //两数之和 < 目标值，小数指针后移
+                lowIdx++;
+            } else {
+                return new int[]{nums[lowIdx], nums[highIdx]}; //两数之和 == 目标值，返回这两个数
+            }
+        }
+        return new int[0];
+    }
 }
