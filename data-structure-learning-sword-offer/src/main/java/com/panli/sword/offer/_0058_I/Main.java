927be56bff940e08f9b3a9f33a9f3fef506bc382
panli004 <panli004@ke.com>
2021-03-06 19:24:44 +0800

    sword-offer: 翻转单词顺序

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0058_I/Main.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/_0058_I/Main.java
@@ -7,4 +7,20 @@ package com.panli.sword.offer._0058_I;
  * @date 2021-02-23
  */
 public class Main {
+
+    /**
+     * 单次翻转
+     *
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        String[] wordArrs = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = wordArrs.length - 1; i >= 0; i--) {
+            if (wordArrs[i].equals("")) continue;
+            sb.append(wordArrs[i]).append(" ");
+        }
+        return sb.toString().trim();
+    }
 }
