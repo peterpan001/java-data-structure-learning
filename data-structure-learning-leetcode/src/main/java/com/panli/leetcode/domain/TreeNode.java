af6465c33192941e56a34be9a1a300887fe23ffb
panli004 <panli004@ke.com>
2021-03-18 08:57:15 +0800

    leetcode:二叉树的层序遍历

---
+++ data-structure-learning-leetcode/src/main/java/com/panli/leetcode/domain/TreeNode.java
@@ -0,0 +1,24 @@
+package com.panli.leetcode.domain;
+
+/**
+ * @author lipan
+ * @date 2021-03-18
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ data-structure-learning-leetcode/src/main/java/com/panli/leetcode/medium_difficult/_0102/Solution.java
@@ -0,0 +1,57 @@
+package com.panli.leetcode.medium_difficult._0102;
+
+import com.panli.leetcode.domain.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层序遍历
+ *
+ * @author lipan
+ * @date 2021-03-18
+ */
+public class Solution {
+
+    /**
+     * 二叉树的层序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        // 如果根节点为空，直接返回
+        if (root == null) {
+            return lists;
+        }
+        // 声明队列，利用队列先进先出特性
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // 根节点进队列
+        queue.add(root);
+        // 队列不为空，则一直循环
+        while (!queue.isEmpty()) {
+            // 获取当前层次的元素个数
+            int size = queue.size();
+            // 声明指标，用于分割当前层次队列节点
+            int cur = 0;
+            List<Integer> list = new ArrayList<Integer>();
+            // 当前层次节点出队列，退出条件的当前层次队列所有节点已经出队列了
+            while (cur < size) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                cur++;
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
