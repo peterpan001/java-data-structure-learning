b9d3bc8d0b3c3d3293dfae57f6343af852da4224
panli004 <panli004@ke.com>
2021-03-22 15:50:07 +0800

    sword offer: m * n 数组中找 topK

--- data-structure-learning-interview/src/main/java/com/panli/interview/two_dimensional_array_topk/Solution.java
+++ data-structure-learning-interview/src/main/java/com/panli/interview/two_dimensional_array_topk/Solution.java
@@ -28,36 +28,32 @@ public class Solution {
 
 
     public static int[] findTopMaxK(int[][] arr, int k) {
-        Queue<Integer> heap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+        Queue<Integer> heap = new PriorityQueue<Integer>(new Comparator<Integer>() {
             @Override
             public int compare(Integer x1, Integer x2) {
-                return Integer.compare(x1, x2);
+                return Integer.compare(x2, x1);
             }
         });
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 heap.add(arr[i][j]);
-                if (heap.size() > k) {
-                    heap.poll();
-                }
             }
         }
-        int[] res = new int[heap.size()];
-        int idx = 0;
-        for (int no : heap) {
-            res[idx++] = no;
+        int res[] = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll();
         }
         return res;
     }
 
     public static void main(String[] args) {
         int[][] arr = {{4, 1, 8, 9}, {5, 2, 7, 8}, {3, 3, 2}};
-        int[] maxArr = findTopMaxK(arr, 1);
+        int[] maxArr = findTopMaxK(arr, 3);
         for (int i = 0; i < maxArr.length; i++) {
             System.out.println(maxArr[i]);
         }
         System.out.println("-------------------");
-        int[] minArr = findTopMinK(arr, 1);
+        int[] minArr = findTopMinK(arr, 3);
         for (int i = 0; i < minArr.length; i++) {
             System.out.println(minArr[i]);
         }
