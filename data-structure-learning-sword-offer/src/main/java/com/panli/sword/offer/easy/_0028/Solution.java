1c67d83f3e5496596d5d267aea5a9e5dd511e6ca
panli004 <panli004@ke.com>
2021-04-09 18:18:13 +0800

    sword offer：对称的二叉树

---
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/easy/_0028/Solution.java
@@ -0,0 +1,26 @@
+package com.panli.sword.offer.easy._0028;
+
+import com.panli.sword.offer.domain.TreeNode;
+
+/**
+ * @author lipan
+ * @date 2021-04-09
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
