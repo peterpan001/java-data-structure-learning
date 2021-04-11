819cf93fcdca6b40d5ac24df39b8282cd32e5023
panli004 <panli004@ke.com>
2021-04-11 16:56:57 +0800

    sword offer：丑数

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/medium_difficulty/_0049/Main.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/medium_difficulty/_0049/Main.java
@@ -7,4 +7,26 @@ package com.panli.sword.offer.medium_difficulty._0049;
  * @date 2021-03-07
  */
 public class Main {
+    public int nthUglyNumber(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int uglyArr[] = new int[n + 1];
+        uglyArr[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int nums2 = uglyArr[p2] * 2, nums3 = uglyArr[p3] * 3, nums5 = uglyArr[p5] * 5;
+            uglyArr[i] = Math.min(Math.min(nums2, nums3), nums5);
+            if (uglyArr[i] == nums2) {
+                p2++;
+            }
+            if (uglyArr[i] == nums3) {
+                p3++;
+            }
+            if (uglyArr[i] == nums5) {
+                p5++;
+            }
+        }
+        return uglyArr[n];
+    }
 }
