1f19c3379b2bfd8306826b0af23a0e7cadb26533
panli004 <panli004@ke.com>
2021-04-13 14:50:58 +0800

    sword offer：把数组排成最小的树

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/medium_difficulty/_0045/Main.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/medium_difficulty/_0045/Main.java
@@ -1,5 +1,7 @@
 package com.panli.sword.offer.medium_difficulty._0045;
 
+import java.util.Arrays;
+
 /**
  * 把数组排成最小的数
  *
@@ -7,4 +9,23 @@ package com.panli.sword.offer.medium_difficulty._0045;
  * @date 2021-03-07
  */
 public class Main {
+
+    public String minNumber(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return null;
+        }
+        String[] nums2Str = new String[len];
+        for (int i = 0; i < len; i++) {
+            nums2Str[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(nums2Str, (a, b) -> {
+            return (a + b).compareTo(b + a);
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            sb.append(nums2Str[i]);
+        }
+        return sb.toString();
+    }
 }
