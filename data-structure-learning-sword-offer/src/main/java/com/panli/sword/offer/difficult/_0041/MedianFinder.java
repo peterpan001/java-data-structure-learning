a11a2bae1e41b01344199d4b585b5f272bc38836
panli004 <panli004@ke.com>
2021-04-14 16:31:13 +0800

    sword offer：数据流的中位数

--- data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/difficult/_0041/MedianFinder.java
+++ data-structure-learning-sword-offer/src/main/java/com/panli/sword/offer/difficult/_0041/MedianFinder.java
@@ -1,6 +1,5 @@
 package com.panli.sword.offer.difficult._0041;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -11,33 +10,26 @@ import java.util.Queue;
  * @date 2021-03-22
  */
 public class MedianFinder {
-    Queue<Integer> a, b;
+
+    Queue<Integer> small, big;
 
     public MedianFinder() {
-        a = new PriorityQueue<Integer>(new Comparator<Integer>() {
-            public int compare(Integer i1, Integer i2) {
-                return i2 - i1;
-            }
-        }); // 大顶堆，存储较小的一部分
-        b = new PriorityQueue<Integer>(); // 小顶堆，存储较大的一部分
+        small = new PriorityQueue<>(); // 小顶堆，存较大的一部分
+        big = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆，存较小的一部分
     }
 
-    /**
-     * 当 a 和 b 队列元素相等时，优先保证元素落入 a 队列
-     * 当 a 和 b 队列元素不相等时，说明 a 队列元素多，则让元素进入 b 队列
-     * @param num
-     */
     public void addNum(int num) {
-        if (a.size() != b.size()) {
-            a.add(num);
-            b.add(a.poll());
+        if (small.size() == big.size()) {
+            small.add(num);
+            big.add(small.poll());
         } else {
-            b.add(num);
-            a.add(b.poll());
+            big.add(num);
+            small.add(big.poll());
         }
     }
 
     public double findMedian() {
-        return a.size() != b.size() ? a.peek() : (a.peek() + b.peek()) / 2.0;
+        return small.size() == big.size() ? (small.peek() + big.peek()) / 2.0 : big.peek();
     }
+
 }
